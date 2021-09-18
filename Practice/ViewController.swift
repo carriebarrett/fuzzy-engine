5300552a70be41dca529fe1dbdeb0a7c88fd2793
carriebarrett <67287344+carriebarrett@users.noreply.github.com>
2021-09-18 11:33:38 -0700

    Added all basic files

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Carrie Barret on 9/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
